3140a5e3d59ec7e4e6d147409af7a8e04270aeb1
Mayooran-dev <mt25acl@herts.ac.uk>
2026-03-28 20:36:50 +0000

    Created Booking.java in  model

---
+++ OneDrive - University of Hertfordshire/Hertfordshire/2nd Semester/7COM1025-0105-2025 - Programming for Software Engineers/FLC-Booking-System/FLC-Booking-System/src/main/java/flc/model/Booking.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flc.model;
+
+/**
+ *
+ * @author Devar
+ */
+public class Booking {
+    private static int counter = 1;
+    private final int bookingId;
+    private final Member member;
+    private Lesson lesson;
+
+    public Booking(Member member, Lesson lesson) {
+        this.bookingId = counter++;
+        this.member = member;
+        this.lesson = lesson;
+    }
+
+    public int getBookingId() { return bookingId; }
+    public Member getMember() { return member; }
+    public Lesson getLesson() { return lesson; }
+
+    public void setLesson(Lesson lesson) { this.lesson = lesson; }
+
+    @Override
+    public String toString() {
+        return "Booking #" + bookingId + " | " + member.getName() + " | " + lesson;
+    }
